9f2b91153255ac1f6b5cc9692d0322dcf4da3d8c
Aleksandr Szestak <szestak@hotmail.com>
2024-08-28 21:35:19 +0300

    refactor: improve code of function

--- src/index.js
+++ src/index.js
@@ -1,37 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
   let i, symb, tSymb;
-  let arrPureStr=[], sumo=0;
+  let arrPureStr = [], sumo = 0;
 
-  for(i=0;i<str.length;i++){
+  for(i = 0; i < str.length; i++) {
       
       symb  = str[i];
-      tSymb = testSymb(symb,bracketsConfig);
+      tSymb = testSymb(symb, bracketsConfig);
       
       if (tSymb >= 1000){
-          if (arrPureStr[0] === tSymb){
+          if (arrPureStr[0] === tSymb) {
               arrPureStr.shift();
               sumo -= tSymb;
           }
-          else{
+          else {
               arrPureStr.unshift(tSymb)
               sumo += tSymb;
           }
       }
-      else if (tSymb > 0){
+      else if (tSymb > 0) {
           arrPureStr.unshift(tSymb)
           sumo += tSymb;
       }
-      else if (tSymb <0 && arrPureStr[0] === Math.abs(tSymb)){
+      else if (tSymb < 0 && arrPureStr[0] === Math.abs(tSymb)) {
           arrPureStr.shift();
           sumo += tSymb;
       }
-      else if (tSymb <0 && arrPureStr[0] !== Math.abs(tSymb)){
+      else if (tSymb < 0 && arrPureStr[0] !== Math.abs(tSymb)) {
           return false;
       }
 
   }
       
-  if (arrPureStr.length % 2 !==0 || sumo !== 0){
+  if (arrPureStr.length % 2 !== 0 || sumo !== 0) {
       return false;
   }
 
@@ -40,21 +40,21 @@ module.exports = function check(str, bracketsConfig) {
 
 
 // ------------------------------------------------------
-function testSymb(symb, bracketsConfig){
-  let i, rez=0;
+function testSymb(symb, bracketsConfig) {
+  let i, rez = 0;
   
-  for (i=0;i<bracketsConfig.length;i++){
+  for (i = 0; i < bracketsConfig.length; i++) {
       
-      if (symb === bracketsConfig[i][0] && symb === bracketsConfig[i][1]){
-          rez=(i+1)*1000;
+      if (symb === bracketsConfig[i][0] && symb === bracketsConfig[i][1]) {
+          rez = (i + 1) * 1000;
           break;
       }
       else if (symb === bracketsConfig[i][0]){
-          rez=i+1;
+          rez = i + 1;
           break;
       }
-      else if(symb === bracketsConfig[i][1]){
-          rez=-(i+1);
+      else if(symb === bracketsConfig[i][1]) {
+          rez = -(i + 1);
           break;
       }
       
